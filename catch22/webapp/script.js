63a266f21e73b8d5ccbc112803e5877772bdd230
tramadoss <71474481+tramadoss@users.noreply.github.com>
2020-09-29 00:35:57 -0400

    attempted adding word tree

--- catch22/webapp/script.js
+++ catch22/webapp/script.js
@@ -322,3 +322,39 @@ async function drawYesCoal() {
  	var chart = new google.visualization.BarChart(document.getElementById('theYesCoal'));
  	chart.draw(data, options);
 }
+
+
+      function drawCats() {
+        var data = google.visualization.arrayToDataTable(
+          [ ['Phrases'],
+            ['cats are better than dogs'],
+            ['cats eat kibble'],
+            ['cats are better than hamsters'],
+            ['cats are awesome'],
+            ['cats are people too'],
+            ['cats eat mice'],
+            ['cats meowing'],
+            ['cats in the cradle'],
+            ['cats eat mice'],
+            ['cats in the cradle lyrics'],
+            ['cats eat kibble'],
+            ['cats for adoption'],
+            ['cats are family'],
+            ['cats eat mice'],
+            ['cats are better than kittens'],
+            ['cats are evil'],
+            ['cats are weird'],
+            ['cats eat mice'],
+          ]
+        );
+
+        var options = {
+          wordtree: {
+            format: 'implicit',
+            word: 'cats'
+          }
+        };
+
+        var chart = new google.visualization.WordTree(document.getElementById('wordtree_basic'));
+        chart.draw(data, options);
+      }
